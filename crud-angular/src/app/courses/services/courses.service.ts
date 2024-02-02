e9e072ff25977d652b12656dfc7e264cd17be8d6
NUN35 <wesley-nunes-00@hotmail.com>
2024-02-02 11:45:32 -0300

    List of Courses: Spinner (Loading)

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { delay, first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,6 @@ export class CoursesService {
   constructor( private httpClient : HttpClient) { }
 
   list(){
-    return this.httpClient.get<Course[]>(this.API).pipe(tap(courses => console.log(courses)));
+    return this.httpClient.get<Course[]>(this.API).pipe(first(),delay(5000), tap(course => console.log(course)));
   }
 }
